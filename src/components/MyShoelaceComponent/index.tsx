271a9ef0c0302b1733a1a341a85b2767f1ec15ab
dk <hi@mrkha.ng>
2025-06-01 00:27:12 +0000

    tmp workaround against useEffect crash

--- src/components/MyShoelaceComponent/index.tsx
+++ src/components/MyShoelaceComponent/index.tsx
@@ -38,6 +38,8 @@ const PHOTOS = (new Array(20)).fill(1).map((_, i) =>
 );
 
 export const MyShoelaceComponent: FunctionComponent = () => {
+  // @TODO fix preact/hooks useEffect crash and re-enable the block below
+  /*
   useEffect(() => {
     let scrolling = false;
     let scrollAmount = 0;
@@ -67,6 +69,7 @@ export const MyShoelaceComponent: FunctionComponent = () => {
       document.removeEventListener("wheel", onMouseWheel);
     };
   }, []);
+  */
 
   return (
     <>
